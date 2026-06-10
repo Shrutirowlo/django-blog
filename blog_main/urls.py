95b4983c8f05cf69ad163020007d35cd3bc1d704
ShrutiRowlo <shrutirowlo2006@gmail.com>
2026-06-10 13:16:39 +0530

    category crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from blogs import views as BlogViews
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
@@ -14,6 +15,8 @@ urlpatterns = [
     path('blogs/search/',BlogViews.search,name='search'),
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
-    path('logout/',views.logout,name='logout')
+    path('logout/',views.logout,name='logout'),
+
+    path('dashboard/',include('dashboards.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
